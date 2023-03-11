0eb9a78d1a5c8f8f740d28ba2f0e6262e6a94d8b
Miguel Lamas <lamasmigs@yahoo.co.uk>
2023-03-11 01:42:14 +0000

    Sorted Hooks, Destructuring, fetch API for Data Fetching

--- src/App.js
+++ src/App.js
@@ -1,9 +1,27 @@
+import React, { useState, useEffect } from 'react';
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap";
 import Filters from "./components/Filters/Filters";
 import Cards from "./components/Cards/Cards";
 
 function App() {
+  let [pageNumber, setPageNumber] = useState(1);
+  let [fetchedData, updateFetchedData] = useState([])
+  let { info, results } = fetchedData;
+  
+  let api = `https://rickandmortyapi.com/api/character/?page=${pageNumber}`;
+
+  // Use an Immediately Invoked Function Expression (IIFE) - runs as soon as it's defined.
+  useEffect(() => {
+
+    (async function (){
+      let data = await fetch(api).then(res => res.json())
+      updateFetchedData(data);
+    })()
+
+
+  },[api]);
+
   return (
     <div className="App">
       <h1 className="text-center ubuntu my-4">

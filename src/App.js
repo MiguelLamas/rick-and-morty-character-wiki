92106e7668f3e9ede66c8412ad87c1238833c979
Miguel Lamas <lamasmigs@yahoo.co.uk>
2023-03-15 19:26:07 +0000

    Created Search Bar Component and Styling, also set it so pagination always begins on Page 1 when you search. I also wanted to preventDefault() behaviour like refreshing page when you onClick on Search button

--- src/App.js
+++ src/App.js
@@ -4,13 +4,15 @@ import "bootstrap/dist/js/bootstrap";
 import Filters from "./components/Filters/Filters";
 import Cards from "./components/Cards/Cards";
 import Pagination from './components/Pagination/Pagination';
+import Search from './components/Search/Search';
 
 function App() {
   let [pageNumber, setPageNumber] = useState(1);
+  let [search, setSearch] = useState("");
   let [fetchedData, updateFetchedData] = useState([])
   let { info, results } = fetchedData;
 
-  let api = `https://rickandmortyapi.com/api/character/?page=${pageNumber}`;
+  let api = `https://rickandmortyapi.com/api/character/?page=${pageNumber}&name=${search}`;
 
   // Use an Immediately Invoked Function Expression (IIFE) - runs as soon as it's defined as per below.
   useEffect(() => {
@@ -28,7 +30,7 @@ function App() {
       <h1 className="text-center ubuntu my-4">
         Rick & Morty Character <span className="text-primary">WiKi</span>
       </h1>
-
+    <Search setPageNumber={setPageNumber} setSearch={setSearch} />
       <div className="container">
         <div className="row">
           <div className="col-3">

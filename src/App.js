b182014cc529893e5b7552c71ac009d00ba9826f
Miguel Lamas <lamasmigs@yahoo.co.uk>
2023-03-18 17:15:27 +0000

    Installed react-router-dom, creating navbar component, styling with Bootstrap

--- src/App.js
+++ src/App.js
@@ -1,18 +1,18 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap";
 import Filters from "./components/Filters/Filters";
 import Cards from "./components/Cards/Cards";
-import Pagination from './components/Pagination/Pagination';
-import Search from './components/Search/Search';
-
+import Pagination from "./components/Pagination/Pagination";
+import Search from "./components/Search/Search";
+import Navbar from "./components/Navbar/Navbar";
 
 function App() {
   let [pageNumber, setPageNumber] = useState(1);
   let [search, setSearch] = useState("");
   let [status, setStatus] = useState("");
   let [gender, setGender] = useState("");
-  let [species, setSpecies] = useState("")
+  let [species, setSpecies] = useState("");
 
   let [fetchedData, updateFetchedData] = useState([]);
   let { info, results } = fetchedData;
@@ -21,41 +21,37 @@ function App() {
 
   // Use an Immediately Invoked Function Expression (IIFE) - runs as soon as it's defined as per below.
   useEffect(() => {
-
-    (async function (){
-      let data = await fetch(api).then(res => res.json())
+    (async function () {
+      let data = await fetch(api).then((res) => res.json());
       updateFetchedData(data);
-    })() 
-
-
-  },[api]);
+    })();
+  }, [api]);
 
   return (
     <div className="App">
-      <h1 className="text-center ubuntu my-4">
-        Rick & Morty Character <span className="text-primary">WiKi</span>
-      </h1>
-    <Search setPageNumber={setPageNumber} setSearch={setSearch} />
+      <Navbar />
+
+      <Search setPageNumber={setPageNumber} setSearch={setSearch} />
       <div className="container">
         <div className="row">
-             <Filters
-             setSpecies={setSpecies}
-             setGender={setGender}
-             setStatus={setStatus} 
-             setPageNumber={setPageNumber} />
+          <Filters
+            setSpecies={setSpecies}
+            setGender={setGender}
+            setStatus={setStatus}
+            setPageNumber={setPageNumber}
+          />
           <div className="col-8">
             <div className="row">
-              <Cards results={results}/>
-              
+              <Cards results={results} />
             </div>
           </div>
         </div>
       </div>
-        <Pagination 
-        info={info} 
-        pageNumber={pageNumber} 
-        setPageNumber={setPageNumber}    
-        />
+      <Pagination
+        info={info}
+        pageNumber={pageNumber}
+        setPageNumber={setPageNumber}
+      />
     </div>
   );
 }

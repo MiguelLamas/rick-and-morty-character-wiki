c608b6349f7bf80ae61c694e4a5e0cd6d654b28a
Miguel Lamas <lamasmigs@yahoo.co.uk>
2023-03-20 23:42:23 +0000

    Added useState and useEffect Hooks to Episodes Component.

--- src/Pages/Episodes.js
+++ src/Pages/Episodes.js
@@ -1,11 +1,52 @@
-import React from 'react'
+import React, { useState, useEffect } from "react";
+import Cards from "../components/Cards/Cards";
 
+// Destructure INFO in OBJECT Response from API
 const Episodes = () => {
-  return (
-    <div>
-      The Episodes Are Here
-    </div>
-  )
-}
+  let [id, setId] = useState(1);
+  let [info, setInfo] = useState([]);
+  let [results, setResults] = useState([]);
+  let { air_date, name } = info;
 
-export default Episodes
+  let api = `https://rickandmortyapi.com/api/episode/${id}`;
+
+  // Inside useEffect Hook add a an IIFE (Immediately Invoked Function Expression)
+  // a JavaScript function that runs as soon as it is defined.
+  useEffect(() => {
+    (async function () {
+      let data = await fetch(api).then((res) => res.json());
+      setInfo(data);
+
+      let details = await Promise.all(
+        data.characters.map((items) => {
+          return fetch(items).then((res) => res.json());
+        })
+      );
+      setResults(details);
+    })();
+  }, [api]);
+
+  return (
+    <div className="container">
+      <div className="row mb-4 ">
+        <h1 className="text-center mb-3">
+          Episode :{" "}
+          <span className="text-primary">{name === "" ? "Unknown" : name}</span>
+        </h1>
+        <h5 className="text-center">
+          Air Date - {air_date === "" ? "Unknown" : air_date}
+        </h5>
+      </div>
+      <div className="row">
+        <div className="col-3">Pick Episodes</div>
+        <div className="col-8">
+          <div className="row">
+            <Cards results={results} />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Episodes;
